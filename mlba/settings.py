ebee0caff15e0a70585645207c5bad9191286091
carmuluba <muluba@gmail.com>
2022-06-03 18:08:53 +0100

    Added files and changes required for deployment to heroku

--- mlba/settings.py
+++ mlba/settings.py
@@ -166,7 +166,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mlba/media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
